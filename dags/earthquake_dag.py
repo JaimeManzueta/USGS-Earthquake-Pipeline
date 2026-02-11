bd9f0615d3b40daade0aabd72149bbdf561f92a8
Jaime <jaimemanzueta@gmail.com>
2026-02-10 19:19:40 -0500

    Newest Version of the project

--- dags/earthquake_dag.py
+++ dags/earthquake_dag.py
@@ -3,10 +3,12 @@ import requests
 import datetime 
 import pendulum
 
+# Airflow setup with dag, postgreshook, and sql setup----------------------------- 
 from airflow.sdk import task, dag
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
+# Creating the DAG thats going to show up in the airflow server------------------
 
 @dag("Earthquake_dag",
      start_date = pendulum.datetime(2021,1,1),
@@ -16,20 +18,51 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 )
 
-
+# My first task that will extract the Data-------------------------------
 @task
 def extract_data():
-    pass
+    API = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2025-11-01&endtime=2025-11-14&minmagnitude=4.5&orderby=time"
+    response = requests.get(API)
+    extracted = response.json()
 
-
-
-
-@task
-def transform_data():
-    pass
+    return extracted
     
 
 
+# My Second Task that will Transform the raw Data into clean Data------------------
+@task
+# FIX: data_stored - it doasnt work with the function - cant implement it 
+def transform_data():
+    if data.get("metadata", {}).get("status") == 200:
+        # turning the data into a list
+        data_stored = []
+
+        # Creating a Loop for the data:
+        for feature in data.get("features", []):
+            properties = feature.get("properties", {})
+
+
+            
+            magnitude = properties.get["mag"]
+            place = properties.get["place"]
+            nst = properties.get["nst"]
+            felt = properties.get["felt"]
+
+
+            # store it into the data_stored
+            data_that_stored = {
+                "magnitude: ": magnitude,
+                "Place: ": place,
+                "nst: ": nst,
+                "felt: ": felt
+            }
+            data_stored.append(data_that_stored)
+
+
+        return data_stored
+
+
+# Processing the Sql in order to automate it into the DB---------------------
 
 def sql_processing_quakes():
     create_quakes_table = SQLExecuteQueryOperator(
@@ -49,9 +82,12 @@ def sql_processing_quakes():
           ); """,    
     )
 
-
+# Loading the Clean Data into DB-------------------------------------------
 def load_data():
     pass
+    
 
 
 
+
+# Combining everything together------------------------------------------
--- dags/testing_dag_data.py
+++ dags/testing_dag_data.py
@@ -1,25 +1,73 @@
+# All of the Libraries-----------------------------------
 import requests
 import os 
 import json
+import pandas as pd 
 
+# Extracted Raw Data----------------------------------------------------
 
-
-
+# Access the data using request to access the html of the api website 
 def get_data(url):
-    
+    response = requests.get(url)
+    # Convert it into .json 
+    get_data = response.json()
 
-    response = requests.request("GET", url)
+    return get_data
 
-    print(response)
+
+# Transforming Raw Data Into Clean Data----------------------------------------
+def transform_data(data):
+    # If the metadata status is there (200) then give me the data
+    if data.get("metadata", {}).get("status") == 200:
+        # initialize in A list first 
+        data_stored = []
+        # accessing the data's dictionary on the api web - {only access to the first data} - Used some AI for help on this 
+        for feature in data.get("features", []):
+            properties = feature.get("properties", {})        
+
+        # gathering only the important and relevent data from the dictionary
+            magnitude = properties.get("mag")
+            place = properties.get("place")
+            nst = properties.get("nst")
+            felt = properties.get("felt")
+
+
+        # store it into the data_stored
+            current_record = {
+                "magnitude: ": magnitude,
+                "Place: ": place,
+                "nst: ": nst,
+                "felt: ": felt
+            }
+            
+            data_stored.append(current_record)
+
+
+        return data_stored
+
     
+# Loading the Clean Data into the DB (postgres)---------------------------------------
+
+def Loading():
+    pass
+
+
+
+
+
+
+
+
+
+
   
-
+# Gathering everything inside this main--------------------------------------------- 
 def main():
+    api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2025-11-01&endtime=2025-11-14&minmagnitude=4.5&orderby=time"
+    fetched_data = get_data(api)
+    trans_data = transform_data(fetched_data)
 
-    get_data("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2025-11-01&endtime=2025-11-14&minmagnitude=4.5&orderby=time")
-
-
-
+    print(trans_data)
 
 
 
